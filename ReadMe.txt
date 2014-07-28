4d3db1142884a0f71f4e1eaf5235d4b7fed62c65
Kuan Bartel <kjbartel@users.noreply.github.com>
2014-07-28 15:08:30 +0900

    Copy of ConnectionDialog project from https://github.com/utsavberi/IDCardManagement-Designer/tree/master/IDCardManagement/ConnectionDialog.

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+Microsoft Visual Studio Connection Dialog
+=========================================
+This is the Visual Studio connection dialog originally released by Microsoft but no longer available on their website.
+
+Details of the dialog are outlined in this msdn blog entry:
+http://blogs.msdn.com/b/vsdata/archive/2010/02/02/data-connection-dialog-source-code-is-released-on-code-gallery.aspx
+The original project site can also be viewed via the Internet Archive's Way Back Machine:
+https://web.archive.org/web/20100216143342/http://code.msdn.microsoft.com/Connection
+
+If all you need is the prebuilt dialog then use nuget:
+https://www.nuget.org/packages/DataConnectionDialog/
+
+This copy was obtained from:
+https://github.com/utsavberi/IDCardManagement-Designer/tree/master/IDCardManagement/ConnectionDialog
---
+++ Sample/Sample.cs
@@ -0,0 +1,82 @@
+﻿//------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation">
+//      Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Data.ConnectionUI;
+
+namespace Sample
+{
+	public class Sample
+	{
+		// Sample 1: 
+		[STAThread]
+		static void Main(string[] args)
+		{
+			DataConnectionDialog dcd = new DataConnectionDialog();
+			DataConnectionConfiguration dcs = new DataConnectionConfiguration(null);
+			dcs.LoadConfiguration(dcd);
+
+			if (DataConnectionDialog.Show(dcd) == DialogResult.OK)
+			{
+				// load tables
+				using (SqlConnection connection = new SqlConnection(dcd.ConnectionString))
+				{
+					connection.Open();
+					SqlCommand cmd = new SqlCommand("SELECT * FROM sys.Tables", connection);
+
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							Console.WriteLine(reader.HasRows);
+						}
+					}
+
+				}
+			}
+
+			dcs.SaveConfiguration(dcd);
+		}
+
+		// Sample 2: 
+		//[STAThread]
+		//static void Main(string[] args)
+		//{
+		//    DataConnectionDialog dcd = new DataConnectionDialog();
+		//    DataConnectionConfiguration dcs = new DataConnectionConfiguration(null);
+		//    dcs.LoadConfiguration(dcd);
+		//    //dcd.ConnectionString = "Data Source=ziz-vspro-sql05;Initial Catalog=Northwind;Persist Security Info=True;User ID=sa;Password=Admin_007";
+
+
+		//    if (DataConnectionDialog.Show(dcd) == DialogResult.OK)
+		//    {
+		//        // load tables
+		//        using (SqlConnection connection = new SqlConnection(dcd.ConnectionString))
+		//        {
+		//            connection.Open();
+		//            SqlCommand cmd = new SqlCommand("SELECT * FROM sys.Tables", connection);
+
+		//            using (SqlDataReader reader = cmd.ExecuteReader())
+		//            {
+		//                while (reader.Read())
+		//                {
+		//                    Console.WriteLine(reader.HasRows);
+		//                }
+		//            }
+
+		//        }
+		//    }
+
+		//    dcs.SaveConfiguration(dcd);
+		//}
+	}
+}
